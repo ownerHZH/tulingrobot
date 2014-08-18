32e8c47ad921269b73f3dd4566de124dc0305c93
ownerHZH <owners.hzh@gmail.com>
2014-08-18 19:27:48 +0800

    实体

---
+++ src/com/owner/entity/Data.java
@@ -0,0 +1,20 @@
+package com.owner.entity;
+
+import java.util.List;
+
+public class Data {
+    private long code;
+    private String text;
+    private String url;
+    private List<Novel> novels;
+    private List<News> newss; 
+    private List<App> apps;
+    private List<Train> trains;
+    private List<Flight> flights;
+    private List<Groupon> groupons;
+    private List<Privilege> privileges;
+    private List<Hotel> hotels;
+    private List<Lottery> lotterys;
+    private List<Price> prices;
+    private List<Restaurant> restaurants;
+}
